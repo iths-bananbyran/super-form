49cda5836911739784a2734ae2333b7917df3dda
iths-anna-boberg <amboberg@gmail.com>
2020-10-28 20:24:39 +0100

    changes in sql

--- super-form.php
+++ super-form.php
@@ -23,8 +23,8 @@ function superform_install() {
     $sql = "CREATE TABLE $table_name (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
         time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-        first_name tinytext NOT NULL,
-        last_name tinytext NOT NULL,
+        first_name text NOT NULL,
+        last_name text NOT NULL,
         email text NOT NULL,
         PRIMARY KEY  (id)
         ) $charset_collate;";
