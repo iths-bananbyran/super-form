029a98fe25dd26fa243c8c9bae6aa595a82394d9
iths-anna-boberg <amboberg@gmail.com>
2020-10-28 20:03:09 +0100

    format fix

--- super-form.php
+++ super-form.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: Super Form
 * Plugin URI: https://iths-bananbyran.github.io/banana-agency-website/
-* Description: Create a form plugin for wordpress
+* Description: Creates a simple contactform you can insert to pages.
 * Version: 1.0
 * Author: Bananbyrån
 */
@@ -33,24 +33,26 @@ function superform_install() {
         dbDelta( $sql );
         
         add_option( 'superform_db_version', $superform_db_version );
-    }
+}
     
-    function superform_form(){
-        global $wpdb;
-        $table_name = $wpdb->prefix . 'superform';
+function superform_form(){
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'superform';
         
-        if (isset($_POST['superform-submitted'])){
-            $first_name = sanitize_text_field( $_POST["first_name"] );
-            $last_name = sanitize_text_field( $_POST["last_name"] );
-            $email = sanitize_email( $_POST["email"] );
+    if (isset($_POST['superform-submitted'])){
+        $first_name = sanitize_text_field( $_POST["first_name"] );
+        $last_name = sanitize_text_field( $_POST["last_name"] );
+        $email = sanitize_email( $_POST["email"] );
             
-            $wpdb->insert($table_name, array(
-                'time'=> date("Y-m-d H:i:s"),
-                'first_name'=>$first_name,
-                'last_name'=>$last_name,
-                'email'=>$email,
+        $wpdb->insert($table_name, array(
+            'time'=> date("Y-m-d H:i:s"),
+            'first_name'=>$first_name,
+            'last_name'=>$last_name,
+            'email'=>$email,
             ));
-            echo "<p><em>Thank you $first_name</em>, we will get in touch with you!</p>";
+        
+        echo "<p><em>Thank you $first_name</em>, we will get in touch with you!</p>";
+
         } else {
             echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
             echo '<p>';
@@ -66,52 +68,57 @@ function superform_install() {
             echo '<p><input type="submit" name="superform-submitted" value="Send"></p>';
             echo '</form>';
         }
-    }
+}
     
-    function superform_menu() {
-        add_menu_page(
-            "Superform Dashboard", 
-            "Superform Dashboard", 
-            "manage_options",
-            "superform",
-            "display_superform_entries");
-        }
+function superform_menu() {
+    add_menu_page(
+        "Superform Dashboard", 
+        "Superform Dashboard", 
+        "manage_options",
+        "superform",
+        "display_superform_entries");
+}
         
-        function display_superform_entries(){
-            global $wpdb;
-            $table_name = $wpdb->prefix . 'superform';
-            $retrieve_entries = $wpdb->get_results( "SELECT * FROM $table_name" );
+function display_superform_entries(){
+    
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'superform';
+    $retrieve_entries = $wpdb->get_results( "SELECT * FROM $table_name" );
             
-            echo "<h2>Superform Dashboard</h2>";
-            echo "<p>Use the short code [superform] to add the form to your page!<p>";
+    echo "<h2>Superform Dashboard</h2>";
+    echo "<p>Use the short code [superform] to add the form to your page!<p>";
             
-            if ($retrieve_entries){
-                echo "<h2>Your entries so far:</h2>";
-                echo "<table><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th>";
+    if ($retrieve_entries){
+        
+        echo "<h2>Your entries so far:</h2>";
+        echo "<table><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th>";
                 
-                foreach ($retrieve_entries as $entry) {
-                    echo "<tr>";
-                    echo "<td>$entry->id</td>";
-                    echo "<td>$entry->first_name</td>";
-                    echo "<td>$entry->last_name</td>";
-                    echo "<td>$entry->email</td>";
-                    echo "</tr>";
-                }
+        foreach ($retrieve_entries as $entry) {
+            
+            echo "<tr>";
+            echo "<td>$entry->id</td>";
+            echo "<td>$entry->first_name</td>";
+            echo "<td>$entry->last_name</td>";
+            echo "<td>$entry->email</td>";
+            echo "</tr>";
+        }
                 
-                echo "</table>";
-            } else {
+        echo "</table>";
+
+        } else {
                 echo "<p>No entries found!</p>";
             }
-        }
+}
         
-        function superform_shortcode() {
-            ob_start();
-            superform_form();
-            return ob_get_clean();
-        }
+function superform_shortcode() {
+    
+    ob_start();
+    superform_form();
+    return ob_get_clean();
+}
         
-        add_action("admin_menu", "superform_menu");
-        register_activation_hook( __FILE__, 'superform_install' );
-        add_action('wp_enqueue_scripts','enqueue_related_pages_scripts_and_styles');
-        add_shortcode('superform', 'superform_shortcode');
+add_action("admin_menu", "superform_menu");
+register_activation_hook( __FILE__, 'superform_install' );
+add_action('wp_enqueue_scripts','enqueue_related_pages_scripts_and_styles');
+add_shortcode('superform', 'superform_shortcode');
         
