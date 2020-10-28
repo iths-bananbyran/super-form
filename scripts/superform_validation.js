2c9f7717f28bd00b005eff8f3201ae09025e0aa1
iths-olle-forsslof <54362382+iths-olle-forsslof@users.noreply.github.com>
2020-10-28 22:06:16 +0100

    adjusted validation and added messages

--- scripts/superform_validation.js
+++ scripts/superform_validation.js
@@ -1,75 +1,103 @@
 const surname = document.querySelector("#surname")
 const lastname = document.querySelector("#lastname")
 const email = document.querySelector("#email")
-
 const submit = document.querySelector("#submit")
-
-surname.addEventListener("blur", (event) => {
-    validateText(event, "surname")
-    validateCss(surnameValidated, surname)
-})
-
-lastname.addEventListener("blur", (event) => {
-    validateText(event, "lastname")
-})
-
-email.addEventListener("blur", (event) => {
-    validateEmail(event)
-})
-
-submit.addEventListener("blur", (event) => {
-    validateSubmit(event)
-})
 
 let surnameValidated = false
 let lastnameValidated = false
 let emailValidated = false
 
+surname.addEventListener("blur", (event) => {
+    validateText(event, "surname")
+    addCss(surnameValidated, surname)
+})
+
+lastname.addEventListener("blur", (event) => {
+    validateText(event, "lastname")
+    addCss(lastnameValidated, lastname)
+})
+
+email.addEventListener("blur", (event) => {
+    validateEmail(event)
+    addCss(emailValidated, email)
+})
+
+submit.addEventListener("click", (event) => {
+    validateSubmit(event)
+})
+
 function validateText(event, input) {
-
-    const textRegex = /[a-zA-Z]+/g
-
+    const textRegex = /[a-zA-Z]+$/g
+    let inputElem;
     if(event.target.value.match(textRegex)) {
-        
         if(input === "surname"){
             surnameValidated = true
-            console.log("surname is Good");
+            inputElem = document.querySelector('#surname-message')
+            inputElem.classList.remove('hidden')
+            inputElem.innerHTML = "Good!"
+           
         } else if(input === "lastname") {
             lastnameValidated = true
-            console.log("Lastname is Good");
+            inputElem = document.querySelector('#lastname-message')
+            inputElem.classList.remove('hidden')
+            inputElem.innerHTML = "Good!"
+           
         }
     } else {
         if(input === "surname"){
             surnameValidated = false
-            console.log("surname Error");
+            if (event.target.value === "") {
+                inputElem = document.querySelector('#surname-message')
+                inputElem.classList.remove('hidden')
+                inputElem.innerHTML = "Please fill in a valid name"
+               
+            } else if (!event.target.value.match(textRegex)) {
+                inputElem = document.querySelector('#surname-message')
+                inputElem.classList.remove('hidden')
+                inputElem.innerHTML = "Please only use letters"
+               
+            } 
         } else if(input === "lastname") {
             lastnameValidated = false
-            console.log("Lastname Error");
+            if (event.target.value === "") {
+                inputElem = document.querySelector('#lastname-message')
+                inputElem.classList.remove('hidden')
+                inputElem.innerHTML = "Please fill in a valid name"
+               
+            } else if (!event.target.value.match(textRegex)) {
+                inputElem = document.querySelector('#lastname-message')
+                inputElem.classList.remove('hidden')
+                inputElem.innerHTML = "Please only use letters"
+               
+            } 
         }
-        
     }
 }
 
 function validateEmail(event) {
-    
     const emailRegex = /^[a-z0-9](?!.*?[^\na-z0-9]{​​2}​​)[^\s@]+@[^\s@]+\.[^\s@]+[a-z0-9]$/
-    
     if(emailRegex.test(event.target.value)) {
-        console.log("Good");
+        emailValidated = true
     } else {
-        console.log("Error");
-    }
+        // if left empty
+        let errorMsg = document.querySelector('#email-message')
+        errorMsg.classList.remove('hidden')
+        errorMsg.innerHTML = "Please fill in a valid email"
 
+        emailValidated = false
+    }
 }
 
 function validateSubmit(event) {
     // inget här just nu
 }
 
-function validateCss(result, element) {
+function addCss(result, element) {
     if(result) {
+        element.classList.remove("invalid")
         element.classList.add("valid")
     } else {
+        element.classList.remove("valid")
         element.classList.add("invalid")
     }
 }
